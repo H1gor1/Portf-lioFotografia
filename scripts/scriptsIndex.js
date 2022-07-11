3c697b6e562502b084d5d064465b4d5de9ac509a
Higor Gabriel Lino Silva <65170741+H1gor1@users.noreply.github.com>
2022-07-10 23:11:21 -0300

    Updates

--- scripts/scriptsIndex.js
+++ scripts/scriptsIndex.js
@@ -1,8 +1,12 @@
-var nmrImg = 0;
-var img = [];
-var imgBlur = [];
-var tempo = 6;
-var contador = 1;
+/*
+    Basicamente o script é para controle das fotos do carrosel de fotos da página inicial, ele muda a imagem sozinho de 6 em 6 segundos e tem botões para que o usuário possa mudar
+*/
+
+var nmrImg = 0; //Variável que controla a imagem atual.
+var img = []; //Vetor que guarda as imagens
+var imgBlur = []; //Vetor que guarda as imagens com blur
+var tempo = 6; //Variável que define os segundos que cada imagem irá mudar
+var contador = 1; //Variável contador que é usada nas funções que mudam a imagem com o click do usuário
 
 img[0] = "./images/img1.jpg";
 img[1] = "./images/img2.jpg";
@@ -12,17 +16,18 @@ imgBlur[0] = "./images/img1Blur.png";
 imgBlur[1] = "./images/img2Blur.png";
 imgBlur[2] = "./images/img3Blur.png";
 
-function imagesInterval(){
-    var contentBackground = document.getElementById("content");
-    document.getElementById("image").src = img[nmrImg];
-    contentBackground.style.backgroundImage = "url(" + imgBlur[nmrImg] + ")";
+function imagesInterval(){ //Esta função é a que muda as imagens a cada 6 segundos sem a interação do usuário
+    var contentBackground = document.getElementById("content"); //A variável contentBackground armazena o elemento content.
+    document.getElementById("image").src = img[nmrImg]; //Troco a src do elemento img que está dentro do content
+    contentBackground.style.backgroundImage = "url(" + imgBlur[nmrImg] + ")"; //Mudo o background do elemento content atráves do css.
 
-    nmrImg = (nmrImg + 1) % img.length;
+    nmrImg = (nmrImg + 1) % img.length; //Aumento a variável que controla a imagem atual e faço resto da divisão pelo tamanho do vetor que armazena as imagens, fazendo que o valor não extrapole o tamanho do vetor.
 }
-setInterval(imagesInterval, tempo*1000);
+setInterval(imagesInterval, tempo*1000); //Seto um intervalo de tempo para que a função seja executada, nesse caso 6 segundos.
 
+
+//Esta função e a próxima são quase iguais, elas basicamente ao usuário clicar no botão passa para a próxima ou para a imagem anterior, faço o controle através de ifs e verifico a variável contador.
 function mudarImg(){
-    console.log(contador);
     var contentBackground = document.getElementById("content");
 
     if (contador == 1){
